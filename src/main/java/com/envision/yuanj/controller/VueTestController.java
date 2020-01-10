b9eea27b433faa3274fb2c84638d409e62b5729d
yuanj1 <yuanj1@corp.21cn.com>
2020-01-10 15:58:15 +0800

    【ADD】与vue项目初次结合，保留之前的HTML

---
+++ src/main/java/com/envision/yuanj/controller/VueTestController.java
@@ -0,0 +1,20 @@
+package com.envision.yuanj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class VueTestController {
+
+    @RequestMapping("/")
+    public String goVueIndex(){
+
+        return "index";
+    }
+
+    @RequestMapping("myApp")
+    public String goSpringIndex(){
+        return "indexMyApp";
+    }
+}
